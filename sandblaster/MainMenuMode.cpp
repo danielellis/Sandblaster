f25f9b336ca2b4c38dec56cc0fd0b9de72fcc092
Daniel Ellis <coug36@gmail.com>
2013-03-10 01:58:42 -0800

    MainMenuMode readability refactor

--- sandblaster/MainMenuMode.cpp
+++ sandblaster/MainMenuMode.cpp
@@ -1,256 +1,251 @@
 #include "MainMenuMode.h"
-#include "GameWindow.h"
+
+#include <assert.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_OpenGL.h>
-#include <iostream>
-#include <vector>
-#include <assert.h>
 
-using namespace std;
+MainMenuMode::MainMenuMode(GameWindow *window) : GameMode(window)
+    , mStart(IE_INITIAL)
+    , mQuit(IE_INITIAL)
+    , mClick(IE_CONTINUOUS)
+    , mButtonPressed(-1)
+    , mNextMode(NULL)
+{ }
 
 MainMenuMode::~MainMenuMode() {
-   mTextureLoader.FreeTexture(&mBackgroundTexture);
-   mTextureLoader.FreeTexture(&mTitleTexture);
+    mTextureLoader.FreeTexture(&mBackgroundTexture);
+    mTextureLoader.FreeTexture(&mTitleTexture);
 
-   for (int i = 0; i < NUM_BUTTONS; ++i) {
-      mTextureLoader.FreeTexture(&mButtons[i].mTexture);
-   }
+    for (int i = 0; i < NUM_BUTTONS; ++i) {
+        mTextureLoader.FreeTexture(&mButtons[i].mTexture);
+    }
 }
 
-
 void MainMenuMode::SetNeighborModes(const std::vector<GameMode *> &modes) {
-   mCharacterSelectMode = modes[0];
+    mCharacterSelectMode = modes[0];
 }
 
 bool MainMenuMode::LoadResources() {
-   mTextureLoader.SetMipMapping(true);
-   mTextureLoader.SetTextureFilter(txBilinear);
+    mTextureLoader.SetMipMapping(true);
+    mTextureLoader.SetTextureFilter(txBilinear);
 
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/background.tga", 
-    &mBackgroundTexture) == TRUE);
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/title.tga", &mTitleTexture) 
-    == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/background.tga", &mBackgroundTexture) == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/title.tga", &mTitleTexture) == TRUE);
 
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/play.tga", 
-    &mButtons[BTN_PLAY].mTexture) == TRUE);
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/options.tga", 
-    &mButtons[BTN_OPTIONS].mTexture) == TRUE);
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/quit.tga", 
-    &mButtons[BTN_QUIT].mTexture) == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/play.tga", &mButtons[BTN_PLAY].mTexture) == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/options.tga", &mButtons[BTN_OPTIONS].mTexture) == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/quit.tga", &mButtons[BTN_QUIT].mTexture) == TRUE);
 
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/play_pressed.tga", 
-    &mButtons[BTN_PLAY].mPressedTexture) == TRUE);
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/options_pressed.tga", 
-    &mButtons[BTN_OPTIONS].mPressedTexture) == TRUE);
-   assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/quit_pressed.tga", 
-    &mButtons[BTN_QUIT].mPressedTexture) == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/play_pressed.tga", &mButtons[BTN_PLAY].mPressedTexture) == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/options_pressed.tga", &mButtons[BTN_OPTIONS].mPressedTexture) == TRUE);
+    assert(mTextureLoader.LoadTextureFromDisk("assets/textures/menu/quit_pressed.tga", &mButtons[BTN_QUIT].mPressedTexture) == TRUE);
 
-   for (int i = 0; i < NUM_BUTTONS; ++i) {
-      int windowWidth = mGameWindow->GetWidth();
-      int windowHeight = mGameWindow->GetHeight();
+    for (int i = 0; i < NUM_BUTTONS; ++i) {
+        int windowWidth = mGameWindow->GetWidth();
+        int windowHeight = mGameWindow->GetHeight();
 
-      mButtons[i].xMin = (int)floor((windowWidth / 2.0f) - 
-       ((mButtons[i].mTexture.Width * (windowWidth / 1600.0)) / 2.0f));
-      mButtons[i].xMax = (int)floor((windowWidth / 2.0f) + 
-       ((mButtons[i].mTexture.Width * (windowWidth / 1600.0)) / 2.0f));
+        mButtons[i].xMin = (int)floor((windowWidth / 2.0f) - 
+            ((mButtons[i].mTexture.Width * (windowWidth / 1600.0)) / 2.0f));
+        mButtons[i].xMax = (int)floor((windowWidth / 2.0f) + 
+            ((mButtons[i].mTexture.Width * (windowWidth / 1600.0)) / 2.0f));
 
-      mButtons[i].yMin = (int)floor(((i + 1) * windowHeight / 4.0f) - 
-       ((mButtons[i].mTexture.Height * (windowHeight / 1200.0f)) / 2.0f));
-      mButtons[i].yMax = (int)floor(((i + 1) * windowHeight / 4.0f) + 
-       ((mButtons[i].mTexture.Height * (windowHeight / 1200.0f)) / 2.0f));
-   }
-   return true;
+        mButtons[i].yMin = (int)floor(((i + 1) * windowHeight / 4.0f) - 
+            ((mButtons[i].mTexture.Height * (windowHeight / 1200.0f)) / 2.0f));
+        mButtons[i].yMax = (int)floor(((i + 1) * windowHeight / 4.0f) + 
+            ((mButtons[i].mTexture.Height * (windowHeight / 1200.0f)) / 2.0f));
+    }
+    return true;
 }
 
 void MainMenuMode::Start() {
-   mNextMode = NULL;
+    mNextMode = NULL;
 
-   InputManager *im = mGameWindow->GetInputManager();
+    InputManager *im = mGameWindow->GetInputManager();
 
-   im->AddKeyEvent(&mStart, SDLK_s);
-   im->AddKeyEvent(&mQuit, SDLK_q);
+    im->AddKeyEvent(&mStart, SDLK_s);
+    im->AddKeyEvent(&mQuit, SDLK_q);
 
-   im->AddMouseEvent(&mClick, IM_MOUSE_LEFT_BUTTON);
+    im->AddMouseEvent(&mClick, IM_MOUSE_LEFT_BUTTON);
 
-   im->ResetAllInputEvents();
+    im->ResetAllInputEvents();
 
-   mGameWindow->SetOrthographicProjection();
-   glEnable(GL_TEXTURE_2D);
-   glEnable(GL_BLEND);
+    mGameWindow->SetOrthographicProjection();
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_BLEND);
 }
 
 void MainMenuMode::Stop() {
-   glDisable(GL_BLEND);
-   glDisable(GL_TEXTURE_2D);
+    glDisable(GL_BLEND);
+    glDisable(GL_TEXTURE_2D);
 
-   mGameWindow->ResetPerspectiveProjection();
+    mGameWindow->ResetPerspectiveProjection();
 }
 
 void MainMenuMode::Update(float timeElapsed) {
-   if (mClick.IsPressed()) {
-      InputManager *im = mGameWindow->GetInputManager();
+    if (mClick.IsPressed()) {
+        InputManager *im = mGameWindow->GetInputManager();
 
-      int mouseX = im->GetMouseX();
-      int mouseY = im->GetMouseY();
+        int mouseX = im->GetMouseX();
+        int mouseY = im->GetMouseY();
 
-      mButtonPressed = -1;
-      for (int i = 0; i < NUM_BUTTONS; ++i) {
-         if (mButtons[i].IsIn(mouseX, mouseY)) {
-            mButtonPressed = i;
-         }
-      }
-   }
-   else {
-      if (mButtonPressed == BTN_PLAY) {
-         mNextMode = mCharacterSelectMode;
-      }
-      else if (mButtonPressed == BTN_OPTIONS) {
-         // TODO: add options menu
-      }
-      else if (mButtonPressed == BTN_QUIT) {
-         mGameWindow->QuitGame();
-      }
-      mButtonPressed = -1;
-   }
+        mButtonPressed = -1;
+        for (int i = 0; i < NUM_BUTTONS; ++i) {
+            if (mButtons[i].IsIn(mouseX, mouseY)) {
+                mButtonPressed = i;
+            }
+        }
+    }
+    else {
+        if (mButtonPressed == BTN_PLAY) {
+            mNextMode = mCharacterSelectMode;
+        }
+        else if (mButtonPressed == BTN_OPTIONS) {
+            // TODO: add options menu
+        }
+        else if (mButtonPressed == BTN_QUIT) {
+            mGameWindow->QuitGame();
+        }
+        mButtonPressed = -1;
+    }
 }
 
 void MainMenuMode::Render() {
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   glMatrixMode(GL_MODELVIEW);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
 
-   int texW, texH;
+    int texW, texH;
 
-   // draw the background
-   glPushMatrix();
-   glLoadIdentity();
-   glBindTexture(GL_TEXTURE_2D, mBackgroundTexture.TextureID);
-   glBegin(GL_QUADS);
-      glTexCoord2i(0, 0);
-      glVertex2i(0, mGameWindow->GetHeight());
+    // draw the background
+    glPushMatrix();
+        glLoadIdentity();
+        glBindTexture(GL_TEXTURE_2D, mBackgroundTexture.TextureID);
+        glBegin(GL_QUADS);
+        glTexCoord2i(0, 0);
+        glVertex2i(0, mGameWindow->GetHeight());
 
-      glTexCoord2i(1, 0);
-      glVertex2i(mGameWindow->GetWidth(), mGameWindow->GetHeight());
+        glTexCoord2i(1, 0);
+        glVertex2i(mGameWindow->GetWidth(), mGameWindow->GetHeight());
 
-      glTexCoord2i(1, 1);
-      glVertex2i(mGameWindow->GetWidth(), 0);
+        glTexCoord2i(1, 1);
+        glVertex2i(mGameWindow->GetWidth(), 0);
 
-      glTexCoord2i(0, 1);
-      glVertex2i(0, 0);
-   glEnd();
-   glPopMatrix();
+        glTexCoord2i(0, 1);
+        glVertex2i(0, 0);
+        glEnd();
+    glPopMatrix();
 
-   // draw the title
-   texW = 7 * mGameWindow->GetWidth() / 8;
-   texH = (int)floor(mTitleTexture.Height * (texW / (float)mTitleTexture.Width));
+    // draw the title
+    texW = 7 * mGameWindow->GetWidth() / 8;
+    texH = (int)floor(mTitleTexture.Height * (texW / (float)mTitleTexture.Width));
 
-   glPushMatrix();
-   glLoadIdentity();
-   glTranslatef(mGameWindow->GetWidth() / 2.0f, 0, 0);
-   glBindTexture(GL_TEXTURE_2D, mTitleTexture.TextureID);
-   glBegin(GL_QUADS);
-      glTexCoord2i(0, 0);
-      glVertex2f(-texW / 2.0f, (float)texH);
+    glPushMatrix();
+        glLoadIdentity();
+        glTranslatef(mGameWindow->GetWidth() / 2.0f, 0, 0);
+        glBindTexture(GL_TEXTURE_2D, mTitleTexture.TextureID);
+        glBegin(GL_QUADS);
+        glTexCoord2i(0, 0);
+        glVertex2f(-texW / 2.0f, (float)texH);
 
-      glTexCoord2i(1, 0);
-      glVertex2f(texW / 2.0f, (float)texH);
+        glTexCoord2i(1, 0);
+        glVertex2f(texW / 2.0f, (float)texH);
 
-      glTexCoord2i(1, 1);
-      glVertex2f(texW / 2.0f, 0);
+        glTexCoord2i(1, 1);
+        glVertex2f(texW / 2.0f, 0);
 
-      glTexCoord2i(0, 1);
-      glVertex2f(-texW / 2.0f, 0);
-   glEnd();
-   glPopMatrix();
+        glTexCoord2i(0, 1);
+        glVertex2f(-texW / 2.0f, 0);
+        glEnd();
+    glPopMatrix();
 
-   // draw play button
-   texW = (int)floor(mButtons[0].mTexture.Width * (mGameWindow->GetWidth() / 1600.0f));
-   texH = (int)floor(mButtons[0].mTexture.Height * (mGameWindow->GetHeight() / 1200.0f));
+    // draw play button
+    texW = (int)floor(mButtons[0].mTexture.Width * (mGameWindow->GetWidth() / 1600.0f));
+    texH = (int)floor(mButtons[0].mTexture.Height * (mGameWindow->GetHeight() / 1200.0f));
 
-   glPushMatrix();
-   glLoadIdentity();
-   glTranslatef(mGameWindow->GetWidth() / 2.0f, mGameWindow->GetHeight() / 4.0f, 0);
+    glPushMatrix();
+        glLoadIdentity();
+        glTranslatef(mGameWindow->GetWidth() / 2.0f, mGameWindow->GetHeight() / 4.0f, 0);
 
-   if (mButtonPressed == BTN_PLAY) {
-      glBindTexture(GL_TEXTURE_2D, 
-       mButtons[BTN_PLAY].mPressedTexture.TextureID);
-   }
-   else {
-      glBindTexture(GL_TEXTURE_2D, mButtons[BTN_PLAY].mTexture.TextureID);
-   }
-   glBegin(GL_QUADS);
-      glTexCoord2i(0, 0);
-      glVertex2f(-texW / 2.0f, texH / 2.0f);
+        if (mButtonPressed == BTN_PLAY) {
+            glBindTexture(GL_TEXTURE_2D, 
+                mButtons[BTN_PLAY].mPressedTexture.TextureID);
+        }
+        else {
+            glBindTexture(GL_TEXTURE_2D, mButtons[BTN_PLAY].mTexture.TextureID);
+        }
+        glBegin(GL_QUADS);
+        glTexCoord2i(0, 0);
+        glVertex2f(-texW / 2.0f, texH / 2.0f);
 
-      glTexCoord2i(1, 0);
-      glVertex2f(texW / 2.0f, texH / 2.0f);
-      
-      glTexCoord2i(1, 1);
-      glVertex2f(texW / 2.0f, -texH / 2.0f);
+        glTexCoord2i(1, 0);
+        glVertex2f(texW / 2.0f, texH / 2.0f);
 
-      glTexCoord2i(0, 1);
-      glVertex2f(-texW / 2.0f, -texH / 2.0f);
-   glEnd();
-   glPopMatrix();
+        glTexCoord2i(1, 1);
+        glVertex2f(texW / 2.0f, -texH / 2.0f);
 
-   // draw the options button
-   glPushMatrix();
-   glLoadIdentity();
-   glTranslatef(mGameWindow->GetWidth() / 2.0f, 2 * mGameWindow->GetHeight() / 4.0f, 0);
+        glTexCoord2i(0, 1);
+        glVertex2f(-texW / 2.0f, -texH / 2.0f);
+        glEnd();
+    glPopMatrix();
 
-   texW = (int)floor(mButtons[1].mTexture.Width * (mGameWindow->GetWidth() / 1600.0f));
-   texH = (int)floor(mButtons[1].mTexture.Height * (mGameWindow->GetHeight() / 1200.0f));
+    // draw the options button
+    glPushMatrix();
+        glLoadIdentity();
+        glTranslatef(mGameWindow->GetWidth() / 2.0f, 2 * mGameWindow->GetHeight() / 4.0f, 0);
 
-   if (mButtonPressed == BTN_OPTIONS) {
-      glBindTexture(GL_TEXTURE_2D, 
-       mButtons[BTN_OPTIONS].mPressedTexture.TextureID);
-   }
-   else {
-      glBindTexture(GL_TEXTURE_2D, mButtons[BTN_OPTIONS].mTexture.TextureID);
-   }
-   glBegin(GL_QUADS);
-      glTexCoord2i(0, 0);
-      glVertex2f(-texW / 2.0f, texH / 2.0f);
+        texW = (int)floor(mButtons[1].mTexture.Width * (mGameWindow->GetWidth() / 1600.0f));
+        texH = (int)floor(mButtons[1].mTexture.Height * (mGameWindow->GetHeight() / 1200.0f));
 
-      glTexCoord2i(1, 0);
-      glVertex2f(texW / 2.0f, texH / 2.0f);
-      
-      glTexCoord2i(1, 1);
-      glVertex2f(texW / 2.0f, -texH / 2.0f);
+        if (mButtonPressed == BTN_OPTIONS) {
+            glBindTexture(GL_TEXTURE_2D, 
+                mButtons[BTN_OPTIONS].mPressedTexture.TextureID);
+        }
+        else {
+            glBindTexture(GL_TEXTURE_2D, mButtons[BTN_OPTIONS].mTexture.TextureID);
+        }
+        glBegin(GL_QUADS);
+        glTexCoord2i(0, 0);
+        glVertex2f(-texW / 2.0f, texH / 2.0f);
 
-      glTexCoord2i(0, 1);
-      glVertex2f(-texW / 2.0f, -texH / 2.0f);
-   glEnd();
-   glPopMatrix();
+        glTexCoord2i(1, 0);
+        glVertex2f(texW / 2.0f, texH / 2.0f);
 
-   // draw the quit button
-   glPushMatrix();
-   glLoadIdentity();
-   glTranslatef(mGameWindow->GetWidth() / 2.0f, 3 * mGameWindow->GetHeight() / 4.0f, 0);
+        glTexCoord2i(1, 1);
+        glVertex2f(texW / 2.0f, -texH / 2.0f);
 
-   texW = (int)floor(mButtons[2].mTexture.Width * (mGameWindow->GetWidth() / 1600.0f));
-   texH = (int)floor(mButtons[2].mTexture.Height * (mGameWindow->GetHeight() / 1200.0f));
+        glTexCoord2i(0, 1);
+        glVertex2f(-texW / 2.0f, -texH / 2.0f);
+        glEnd();
+    glPopMatrix();
 
-   if (mButtonPressed == BTN_QUIT) {
-      glBindTexture(GL_TEXTURE_2D, 
-       mButtons[BTN_QUIT].mPressedTexture.TextureID);
-   }
-   else {
-      glBindTexture(GL_TEXTURE_2D, mButtons[BTN_QUIT].mTexture.TextureID);
-   }
-   glBegin(GL_QUADS);
-      glTexCoord2i(0, 0);
-      glVertex2f(-texW / 2.0f, texH / 2.0f);
+    // draw the quit button
+    glPushMatrix();
+        glLoadIdentity();
+        glTranslatef(mGameWindow->GetWidth() / 2.0f, 3 * mGameWindow->GetHeight() / 4.0f, 0);
 
-      glTexCoord2i(1, 0);
-      glVertex2f(texW / 2.0f, texH / 2.0f);
-      
-      glTexCoord2i(1, 1);
-      glVertex2f(texW / 2.0f, -texH / 2.0f);
+        texW = (int)floor(mButtons[2].mTexture.Width * (mGameWindow->GetWidth() / 1600.0f));
+        texH = (int)floor(mButtons[2].mTexture.Height * (mGameWindow->GetHeight() / 1200.0f));
 
-      glTexCoord2i(0, 1);
-      glVertex2f(-texW / 2.0f, -texH / 2.0f);
-   glEnd();
-   glPopMatrix();
+        if (mButtonPressed == BTN_QUIT) {
+            glBindTexture(GL_TEXTURE_2D, 
+                mButtons[BTN_QUIT].mPressedTexture.TextureID);
+        }
+        else {
+            glBindTexture(GL_TEXTURE_2D, mButtons[BTN_QUIT].mTexture.TextureID);
+        }
+        glBegin(GL_QUADS);
+        glTexCoord2i(0, 0);
+        glVertex2f(-texW / 2.0f, texH / 2.0f);
 
-   SDL_GL_SwapBuffers();
+        glTexCoord2i(1, 0);
+        glVertex2f(texW / 2.0f, texH / 2.0f);
+
+        glTexCoord2i(1, 1);
+        glVertex2f(texW / 2.0f, -texH / 2.0f);
+
+        glTexCoord2i(0, 1);
+        glVertex2f(-texW / 2.0f, -texH / 2.0f);
+        glEnd();
+    glPopMatrix();
+
+    SDL_GL_SwapBuffers();
 }
--- sandblaster/MainMenuMode.h
+++ sandblaster/MainMenuMode.h
@@ -1,50 +1,56 @@
 #ifndef MAIN_MENU_MODE_H
 #define MAIN_MENU_MODE_H
 
+#include <vector>
+
 #include "GameMode.h"
+#include "GameWindow.h"
 #include "InputEvent.h"
 #include "TextureLoader.h"
-#include <vector>
 
 const int NUM_BUTTONS = 3;
 
 class MainMenuMode : public GameMode {
 public:
-   MainMenuMode(GameWindow *window) : GameMode(window),
-    mStart(IE_INITIAL), mQuit(IE_INITIAL), mClick(IE_CONTINUOUS),
-    mButtonPressed(-1), mNextMode(NULL) {}
-   ~MainMenuMode();
+    MainMenuMode(GameWindow *window);
+    ~MainMenuMode();
 
-   void SetNeighborModes(const std::vector<GameMode *> &);
-   bool LoadResources();
-   GameMode *CheckForModeChange() {return mNextMode;}
+   void             SetNeighborModes(const std::vector<GameMode *> &);
+   bool             LoadResources();
+   GameMode *       CheckForModeChange() {return mNextMode;}
 
-   void Start();
-   void Stop();
+   void             Start();
+   void             Stop();
 
-   void Update(float timeElapsed);
-   void Render();
+   void             Update(float timeElapsed);
+   void             Render();
 
 protected:
-   struct Button {
-      glTexture mTexture, mPressedTexture;
-      int xMin, xMax, yMin, yMax;
+    struct Button {
+        glTexture mTexture, mPressedTexture;
+        int xMin, xMax, yMin, yMax;
 
-      bool IsIn(int x, int y) 
-       {return x >= xMin && x <= xMax && y >= yMin && y <= yMax;}
-   };
+        bool IsIn(int x, int y)  {
+            return x >= xMin && x <= xMax && y >= yMin && y <= yMax;
+        }
+    };
 
-   enum {BTN_PLAY = 0, BTN_OPTIONS = 1, BTN_QUIT = 2};
+    enum {
+        BTN_PLAY = 0,
+        BTN_OPTIONS = 1,
+        BTN_QUIT = 2
+    };
 
-   InputEvent mStart, mQuit, mClick;
-
-   TextureLoader mTextureLoader;
-   Button mButtons[NUM_BUTTONS];
-   glTexture mBackgroundTexture;
-   glTexture mTitleTexture;
-   GameMode *mCharacterSelectMode, *mNextMode;
-
-   int mButtonPressed;
+    InputEvent      mStart,
+                    mQuit,
+                    mClick;
+    TextureLoader   mTextureLoader;
+    Button          mButtons[NUM_BUTTONS];
+    glTexture       mBackgroundTexture;
+    glTexture       mTitleTexture;
+    GameMode        *mCharacterSelectMode,
+                    *mNextMode;
+    int             mButtonPressed;
 };
 
 #endif
